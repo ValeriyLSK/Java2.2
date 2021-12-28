ee36814a15df6316d6aba6c40ca79732d05ef797
ValeriyLSK <valeriyleschenko@yandex.ru>
2021-12-28 12:24:14 +0300

    Refatoring

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int customersAccount = 321;
-        int deposit = 986;
-        int priceForBonus = 100;
-        int bonus = deposit / priceForBonus;
-        if (deposit <= 1000) {
+        int customersAccount = 100; //начальная сумма на счету
+        int deposit = 1299; //сумма пополнения
+        int priceForBonus = 100; //шаг бонуса
+        int bonus = deposit / priceForBonus; //рассчет бонуса
+        boolean b = (deposit < 1100);
+        if (b) {
             System.out.println("Сумма средств на счете: " + (customersAccount + deposit));
         } else {
             System.out.println("Начислено бонусных рублей: " + bonus + "; Сумма средств на счете: " + (customersAccount + deposit + bonus));
