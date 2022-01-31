cbc397faae09658f549f7cb09816ea8b7ac9e717
ValeriyLSK <valeriyleschenko@yandex.ru>
2022-01-31 16:19:55 +0300

    refactoring 3

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int customersAccount = 100; //начальная сумма на счету
-        int deposit = 1200; //сумма пополнения
-        int priceForBonus = 100; //шаг бонуса
-        int bonus = deposit / priceForBonus; //рассчет бонуса
-        boolean b = (deposit < 1100);
+        int customersAccount = 100;
+        int deposit = 1100;
+        boolean b = (deposit >= 1100);
+        int bonus = deposit / 100;
         int finalCustomersAccount;
         if (b) {
-            finalCustomersAccount = customersAccount + deposit;
-            System.out.println("Сумма средств на счете: " + finalCustomersAccount);
-        } else {
             finalCustomersAccount = customersAccount + deposit + bonus;
-            System.out.println("Начислено бонусных рублей: " + bonus + "; Сумма средств на счете: " + finalCustomersAccount);
-
+            System.out.println("Начислено бонусных рублей: " + bonus + "; На вашем счету: " + finalCustomersAccount + " рублей");
         }
+        else {
+            finalCustomersAccount = customersAccount + deposit;
+            System.out.println("На вашем счету: " + finalCustomersAccount + " рублей");
+        }
+
 
     }
 }
